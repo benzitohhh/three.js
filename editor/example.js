e49aa58daced1c2e6eef40d1d390c9000fb87796
Ben Immanuel <immanuel_ben@yahoo.co.uk>
2017-01-14 03:01:57 +0000

    various stuff

--- editor/example.js
+++ editor/example.js
@@ -9,7 +9,7 @@
 function update( event ) {
   // time is in milliseconds (so increases 1000 per second)
   time = event.time * 0.001; // this increases about 1 per second
-  this.position.y =+ 10 * Math.sin(time); // so a cycle every 6 seconds (2*pi ~= 6)
+  this.position.y += 10 * Math.sin(time); // so a cycle every 6 seconds (2*pi ~= 6)
 }
 
 // 4. Click "play" to view the animation
